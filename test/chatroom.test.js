38054f3ae9e1d2990df10c5c1f136decb09cd323
emily baier <emilyrbaier@gmail.com>
2019-04-09 16:08:21 -0700

    getClient feature of ChatRoom class passes test

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -17,4 +17,9 @@ describe('chat room test', () => {
     it('chat room instance adds a client', () => {
         expect(clientObject.username).toEqual(expect.any(String));
     });
+
+    it('get client object by username', ()=>{
+        const userObj = chatRoom.getClient(clientObject.username);
+        expect(userObj).toEqual(clientObject);
+    });
 });
