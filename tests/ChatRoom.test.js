0612416f9e5c4be608a303b7f7a4c4f65e22a090
Ben Parisot <me@benparisot.com>
2019-04-09 15:05:18 -0700

    write .getClient method and pass test

--- tests/ChatRoom.test.js
+++ tests/ChatRoom.test.js
@@ -5,10 +5,19 @@ describe('chatroom tests', () => {
   beforeEach(() => {
     chatroom = new ChatRoom();
   });
-  
+
   it('assigns random user name to user', () => {
     const client = {};
     chatroom.add(client);
     expect(client.userName).toEqual(expect.any(String));
+  });
+  it('finds a client based on username', () => {
+    const client = {};
+    const client2 = {};
+    const client3 = {};
+    chatroom.add(client);
+    chatroom.add(client2);
+    chatroom.add(client3);
+    expect(chatroom.getClient(client3.userName)).toEqual(client3);
   });
 });
