b29b8328b4977ac4a22111c73e946cfc5b4bbffa
Parker Helland <hellandp@gmail.com>
2019-04-09 15:38:59 -0700

    added server.js and app.js json stuff

---
+++ server.js
@@ -0,0 +1,14 @@
+const net = require('net');
+////////// needs to go to app.js ->
+const server = net.createServer(socket => {
+  console.log('client connected');
+
+  socket.pipe(socket);
+});
+///////////
+
+
+//////// correct
+const app = require('./app');
+app.listen(51324);
+///////// correct
