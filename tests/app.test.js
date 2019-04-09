789ceaef4d607f2b9ebb477577638bcd36c7aae9
Ben Parisot <me@benparisot.com>
2019-04-09 14:14:13 -0700

    write server file, begin tests and chatroom class

---
+++ tests/app.test.js
@@ -0,0 +1,12 @@
+const ChatRoom = require('../lib/app');
+
+const chatRoom = new ChatRoom();
+
+describe('chatroom tests', () => {
+  it('assings random user name', () => {
+    // const userName = returned username after add client username
+    expect(client.username).toEqual('user1');
+
+  });
+
+});
