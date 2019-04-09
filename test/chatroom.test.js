e733e824034e056498cecb2bec3a35b64b24df4e
emily baier <emilyrbaier@gmail.com>
2019-04-09 15:30:06 -0700

    first chatroom test passes, passing username

---
+++ test/chatroom.test.js
@@ -0,0 +1,20 @@
+const ChatRoom = require('../lib/chatroom');
+
+describe('chat room test', () => {
+    let chatRoom = null;
+    let user1 = {};
+    let user2 = {};
+    let user3 = {};
+    let clientObject = null;
+
+    beforeEach(() => {
+        chatRoom = new ChatRoom;
+        clientObject = chatRoom.add(user1);
+        chatRoom.add(user2);
+        chatRoom.add(user3);
+    });
+
+    it('chat room instance adds a client', () => {
+        expect(clientObject.username).toEqual(expect.any(String));
+    });
+});
