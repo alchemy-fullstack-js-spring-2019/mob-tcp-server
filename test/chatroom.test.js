25fc2973833df2ba1cf74155715fab91c68cddce
emily baier <emilyrbaier@gmail.com>
2019-04-09 17:20:28 -0700

    renames username with new username

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -1,25 +1,29 @@
 const ChatRoom = require('../lib/chatroom');
 
 describe('chat room test', () => {
+    
     let chatRoom = null;
     let user1 = {};
-    let user2 = {};
-    let user3 = {};
     let clientObject = null;
 
     beforeEach(() => {
         chatRoom = new ChatRoom;
         clientObject = chatRoom.add(user1);
-        chatRoom.add(user2);
-        chatRoom.add(user3);
     });
 
     it('chat room instance adds a client', () => {
         expect(clientObject.username).toEqual(expect.any(String));
     });
 
-    it('get client object by username', ()=>{
+    it('get client object by username', () => {
         const userObj = chatRoom.getClient(clientObject.username);
         expect(userObj).toEqual(clientObject);
+    });
+
+    it('on chatRoom.rename, return true', () => {
+        const newUserName = 'banana';
+        const nameFromMap = chatRoom.renameClient(clientObject.username, newUserName);
+
+        expect(nameFromMap).toEqual(newUserName);
     });
 });
