8bc146c65783af7f8e37c5ab405e408a4a7842f1
Masmics <cherlocker3113@gmail.com>
2019-04-13 14:13:21 -0700

    rename function passing

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const net = require('net');
-
 const readline = require('readline');
+const chatroom = require('../chatroom');
+const parse = require('../parse.js');
+
+console.log('just storing', chatroom, parse);
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: '🤯 '
 });
+
+// const server = net.createServer(socket => {
+//   console.log('client connected');
+//   socket.pipe(socket);
+// });
 
 const client = net.createConnection(51324, '192.168.1.155', () => {
   console.log('we connected');
@@ -20,5 +28,9 @@ const client = net.createConnection(51324, '192.168.1.155', () => {
 
 client.on('data', data => {
   console.log(data.toString());
+  // const message = parse(data.toString);
+  // console.log(message);
   rl.prompt();
 });
+
+//module.exports = server;
--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -6,7 +6,7 @@ describe('CHAT ROOM TESTS', () => {
     chatroom = new ChatRoom();
   });
 
-  it('stores username in chatroom', () => {
+  it('stores client socket/username in chatroom', () => {
     const client = {};
     const result = chatroom.createUser(client); 
     // result = created object within all users array
@@ -23,9 +23,35 @@ describe('CHAT ROOM TESTS', () => {
 
   it('renames the client username', () => {
     const client = {};
-    const user = chatroom.createUser(client); 
-    const newUser = chatroom.rename(user.username, user.newUserName);
-    console.log(newUser);
-    expect('fake').toEqual('fake');
+    chatroom.createUser(client); 
+    expect(client.username).not.toMatch('Henrietta');
+    chatroom.rename(client.username, 'Henrietta');
+    expect(client.username).toEqual('Henrietta');
   });
+
+  //ryan's example:
+  //const client = {};
+  //   chatroom.add(client);
+  // chatroom.rename(client.username, 'ryan');
+  // const result = chatroom.getClient('ryan');
+  // expect(client).toEqual(result);
+  // expect(client.username).toEqual('ryan');
+
+  // it('gets all', () => {
+  //   client1{};
+  //   client2{};
+  //   client3{}:
+  //GETALL
+  //   const client1 = chatroom.add(client1);
+  //   const client2 = chatroom.add(client2);
+  //   const client3 = chatroom.add(client3);
+  // //I added const client
+  //   const results = chatroom.all();
+  //   expect(results).toContainEqual(client1);
+  //   expect(results).toContainEqual(client2);
+  //   expect(results).toContainEqual(client3);
+
+  //(and some other stuff in chatroom.js I think)
+  //})
+
 });
